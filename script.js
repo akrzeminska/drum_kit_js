72bd260eeec97d3033af90371611c2bb99aaf93b
Adelina Krzemińska <adelina.krzeminska@gmail.com>
2023-04-12 14:13:18 +0200

    added feature with button animation

--- script.js
+++ script.js
@@ -1,85 +1,43 @@
-/*
-----LOGIC----
-Step 1. Select all drums
-Step 2. Add btn press/click event listner to all 
-Step 3. Play sound when btn press/click
-Step 4. Animate
-*/
-
-// Step 1: Get all drums
 let drums = document.querySelectorAll('.drum');
 
-// Step 2.1: Add clik event listener
 drums.forEach(drum => {
     drum.addEventListener('click', () => {
-       playSound(drum.querySelector('.shortcut').textContent)
-       playAnimation(drum.querySelector('.shortcut').textContent)
+       const shortcutContent = drum.querySelector('.shortcut').textContent;
+        playSound(shortcutContent);
+        playAnimation(shortcutContent);
     })
 })
 
-// Step 2.2: Add keypress event listener
 document.addEventListener('keypress', (event)=> {
     playSound(event.key);
-    playAnimation(event.key)
+    playAnimation(event.key);
 })
 
-// Step 3: Play sound when event is triggeredwss
-function playSound(drum) {
-    drum = drum.toLowerCase();
-    switch (drum) {
-        case "w":
-            var audio =  new Audio("assets/sounds/clap.wav")
-            audio.play()
-            break;
-        case "s":
-            var audio =  new Audio("assets/sounds/hihat.wav")
-            audio.play()
-            break;
-        case "d":
-            var audio =  new Audio("assets/sounds/kick.wav")
-            audio.play()
-            break;
-        case "f":
-            var audio =  new Audio("assets/sounds/openhat.wav")
-            audio.play()
-            break;
-        case "g":
-            var audio =  new Audio("assets/sounds/boom.wav")
-            audio.play()
-            break;
-        case "h":
-            var audio =  new Audio("assets/sounds/ride.wav")
-            audio.play()
-            break;
-        case "j":
-            var audio =  new Audio("assets/sounds/snare.wav")
-            audio.play()
-            break;
-        case "k":
-            var audio =  new Audio("assets/sounds/tom.wav")
-            audio.play()
-            break;
-        case "l":
-            var audio =  new Audio("assets/sounds/tink.wav")
-            audio.play()
-            break;
+const sounds = {
+    w: "assets/sounds/clap.wav",
+    s: "assets/sounds/hihat.wav",
+    d: "assets/sounds/kick.wav",
+    f: "assets/sounds/openhat.wav",
+    g: "assets/sounds/boom.wav",
+    h: "assets/sounds/ride.wav",
+    j: "assets/sounds/snare.wav",
+    k: "assets/sounds/tom.wav",
+    l: "assets/sounds/tink.wav"
+};
 
-        default:
-            console.log(drum)
-            break;
-    }
+function playSound(key) {
+    key = key.toLowerCase();
+    const audio = new Audio(sounds[key]);
+    audio.play();
 }
 
-// Step 4: Animate active button
 function playAnimation (currentKey) {
-    console.log(currentKey);
+    const animationTime = 250;
     currentKey = currentKey.toLowerCase();
     let activeButton = document.getElementById(currentKey); 
-
-    console.log(activeButton);
-    activeButton.classList.add("pressed")
+    activeButton.classList.add("pressed");
     
-    setTimeout(function() {
-        activeButton.classList.remove("pressed")
-    }, 250)
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, animationTime)
 }
